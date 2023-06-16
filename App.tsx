bb05e174e7ea0a3560f185fa6e6c2afc10e06ccb
Arunshaik2001 <arunshaik432@gmail.com>
2023-06-16 13:50:00 +0530

    expo notifications demo

--- App.tsx
+++ App.tsx
@@ -1,20 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Platform, StyleSheet, Text, View } from 'react-native';
+import * as Notification from 'expo-notifications';
+
+Notification.setNotificationHandler({
+	handleNotification: async () => ({
+		shouldShowAlert: true,
+		shouldPlaySound: false,
+		shouldSetBadge: false,
+	}),
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	async function handleNotification(delay: number) {
+		console.log('delay', delay);
+		const title = 'Notification Title';
+		const description = 'Notification Body';
+		if (Platform.OS === 'ios') {
+			Notification.requestPermissionsAsync()
+				.then((statusObj) => {
+					if (statusObj.status !== 'granted') {
+						return Notification.requestPermissionsAsync();
+					}
+					return statusObj;
+				})
+				.then((statusObj) => {
+					if (statusObj.status === 'granted') {
+						Notification.scheduleNotificationAsync({
+							content: {
+								title: title,
+								body: description,
+								data: { name: 'arun' },
+							},
+							trigger: { seconds: delay },
+						});
+					}
+				});
+		} else {
+			await Notification.scheduleNotificationAsync({
+				content: {
+					title: title,
+					body: description,
+					data: { name: 'arun' },
+				},
+				trigger: { seconds: delay },
+			});
+		}
+	}
+
+	return (
+		<View style={styles.container}>
+			<Button
+				title='Show Local Notification'
+				onPress={handleNotification.bind(null, 0)}
+			/>
+			<Button
+				title='Show Local Notification with 5sec delay'
+				onPress={handleNotification.bind(null, 5)}
+			/>
+			<StatusBar style='auto' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'space-evenly',
+	},
 });
